1596af77c5849aa4960c6a849a558fe2364b3faa
liporm32 <103882298+liporm32@users.noreply.github.com>
2022-04-17 13:40:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function call() {
 
     if(content == "01000001") {
         alert("molto bene");
-        alert()
+        alert("Complimenti hai indovinato!");
+        alert("https://liporm32.github.io/vittoria.html");
     } else {
         alert("non hai capito il tutto mi sa");
     }
-
 }
