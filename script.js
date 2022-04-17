f177ebe1ae25cd55a67b58ed60e9336c7ff65bd9
liporm32 <103882298+liporm32@users.noreply.github.com>
2022-04-17 13:23:01 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function call() {
+    var content = document.getElementById("code").value;
+
+    if(content == "01000001") {
+        alert("molto bene");
+        alert()
+    } else {
+        alert("non hai capito il tutto mi sa");
+    }
+
+}
